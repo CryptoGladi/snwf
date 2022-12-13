9e62830bddd262cbf890c400f65b30fc4954e1f1
CryptoGladi <mymamchm@mail.ru>
2022-12-13 21:04:23 +0300

    almost done

--- src/protocol/udt.rs
+++ src/protocol/udt.rs
@@ -3,6 +3,7 @@ use crate::{
     stream::progressing_read,
 };
 use async_trait::async_trait;
+use derive_new::new;
 use log::debug;
 use std::{
     fs::read_to_string,
@@ -12,12 +13,14 @@ use std::{
     time::Duration,
 };
 use thiserror::Error;
-use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio::{
     fs::{File, OpenOptions},
     time::timeout,
 };
 use tokio_udt::{UdtConnection, UdtListener};
+
+const TIMEOUT: Duration = Duration::from_millis(1000);
 
 #[derive(Debug, Error)]
 pub enum UdtError {
@@ -31,18 +34,15 @@ pub enum UdtError {
     Connect(#[source] std::io::Error),
 
     #[error("")]
-    A,
-}
+    FileIO(#[source] std::io::Error),
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum NumberOfIterations {
-    Value(u32),
-    Infinity,
+    #[error("")]
+    ConnectionLost,
 }
 
 #[derive(Debug)]
 pub struct UdtConfig {
-    number_of_iterations: NumberOfIterations,
+
 }
 
 #[async_trait]
@@ -57,64 +57,23 @@ pub trait UdtSender: CoreSender {
 
 #[async_trait]
 impl UdtSender for Sender {
-    /* ЭТО ДЛЯ ПОЛУЧЕНИЕ ФАЙЛОВ
-    async fn udt_send_files(&mut self, config: UdtConfig) -> Result<(), UdtError> {
-        debug!("starting udt send files...");
-
-        let port = self.get_port();
-        let udt_listener = match UdtListener::bind((Ipv4Addr::UNSPECIFIED, port).into(), None).await {
-            Ok(result) => result, // TODO Сделать из этого макрос и как отдельную библиотеку
-            Err(error) => return Err(UdtError::Bind(error))
-        };
-        debug!("done udt listenet bind");
-
-        let mut value_iterations = match config.number_of_iterations {
-            NumberOfIterations::Value(value) => value,
-            _ => 0, // is infinity
-        };
-
-        loop {
-            if value_iterations == 0 && config.number_of_iterations != NumberOfIterations::Infinity {
-                debug!("done send files in udt!");
-                return Ok(());
-            }
-
-            let (socket_addr, udt_client) = match udt_listener.accept().await {
-                Ok(result) => result,
-                Err(error) => return Err(UdtError::Accept(error))
-            };
-            debug!("new udt recipient: {}", socket_addr);
-
-            udt_client.send(msg)
-            //progressing_read(reader, |len, bytes| {
-
-            //});
-
-            value_iterations -= 1;
-        }
-    }
-    */
-
     async fn udt_send_file(&mut self, file: &mut File) -> Result<(), UdtError> {
-        debug!("running udt_send_file...");
-        let port = self.get_port();
-        let target = self.get_target();
-        let information_for_connect = (self.get_target(), self.get_port());
+        debug!("Running udt_send_file...");
+        let info_for_connect = (self.get_target(), self.get_port());
 
-        //let mut connection = UdtConnection::connect((target, port), None).await.map_err(|e| Err(UdtError::Connect(e)))?;
-    
-        let mut connection = UdtConnection::connect(information_for_connect, None).await.unwrap_or_else(|e| Err(UdtError::Connect(e)));
-
-        //f.read_to_string(&mut s).map_err(|e| MyCustomError:FileReadError(e))?;
+        let mut connection = UdtConnection::connect(info_for_connect, None)
+            .await
+            .map_err(UdtError::Connect)?;
 
         let mut reader = BufReader::new(file);
-        debug!("done udt connect");
+        debug!("Done connect in udt");
 
-        if let Err(error) = tokio::io::copy(&mut reader, &mut connection).await {
-            return Err(UdtError::IO(error));
-        }
+        // TODO https://stackoverflow.com/questions/71632833/how-to-continuously-watch-and-read-file-asynchronously-in-rust-using-tokio
+        tokio::io::copy(&mut reader, &mut connection)
+            .await
+            .map_err(UdtError::FileIO)?;
 
-        debug!("done work");
+        debug!("Done send file in udt");
         Ok(())
     }
 
@@ -122,41 +81,42 @@ impl UdtSender for Sender {
         &mut self,
         output: P,
     ) -> Result<(), UdtError> {
-        let port = self.get_port();
-        let udt_listener = match UdtListener::bind((Ipv6Addr::UNSPECIFIED, port).into(), None).await
-        {
-            Ok(result) => result, // TODO Сделать из этого макрос и как отдельную библиотеку
-            Err(error) => return Err(UdtError::Bind(error)),
-        };
+        let udt_listener = UdtListener::bind((Ipv6Addr::UNSPECIFIED, self.get_port()).into(), None).await.map_err(UdtError::Bind)?;
 
-        let mut file = OpenOptions::new()
+        let mut file_for_write = OpenOptions::new()
             .create(true)
             .read(true)
             .write(true)
             .open(output)
             .await
-            .unwrap();
+            .map_err(UdtError::FileIO)?;
 
-        let (addr, mut connection) = udt_listener.accept().await.unwrap();
-        println!("Accepted connection from {}", addr);
-        let mut buffer = Vec::with_capacity(4000);
+        let (addr, mut connection) = udt_listener.accept().await.map_err(UdtError::Accept)?;
+        debug!("Accepted connection from {}", addr);
 
-        // TODO https://stackoverflow.com/questions/71632833/how-to-continuously-watch-and-read-file-asynchronously-in-rust-using-tokio
+        if let Ok(copying) = timeout(TIMEOUT, tokio::io::copy(&mut connection, &mut file_for_write)).await {
+            copying.map_err(UdtError::FileIO)?;
+            return Ok(());
+        };
+        Ok(()) // TODO Подклёчение разовралось или всё файлы успешно доставлены?
+
+        /*
         loop {
-            match timeout(Duration::from_millis(100), connection.read_buf(&mut buffer)).await {
+            match timeout(TIMEOUT, connection.read_buf(&mut buffer)).await {
                 Ok(len) => {
                     let Ok(len) = len else {
-                        return Err(UdtError::A);
+                        return Err(UdtError::ConnectionLost);
                     };
 
-                    file.write_all(&buffer[0..len]).await.unwrap();
-                },
+                    file_for_write.write_all(&buffer[0..len]).await.unwrap();
+                }
                 Err(_) => {
-                    file.flush().await.unwrap();
+                    debug!("Timeout end = end send file");
+                    file_for_write.flush().await.unwrap();
                     return Ok(());
                 }
             }
-        }
+        } */
     }
 }
 
