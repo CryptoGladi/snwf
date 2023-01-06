0375d4358fed5fd1ec0420484072dfb81e12605a
CryptoGladi <mymamchm@mail.ru>
2023-01-06 16:41:18 +0300

    commit not work

--- src/protocol/udt/raw.rs
+++ src/protocol/udt/raw.rs
@@ -82,6 +82,7 @@ where
     // Recv file
     debug!("raw_recv_file. Getting file");
 
+    // TODO handshake.unwrap_or(recv_handshake_from_address(socket).await?);
     let handshake = if let Some(handshake) = handshake {
         handshake
     } else {
